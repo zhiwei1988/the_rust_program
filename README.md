c64725959d4407704e109ea4abc4f8790933e05b
ZhiWei Tan <zhiweix1988@gmail.com>
2025-06-01 22:14:52 +0800

    添加笔记和示例代码

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # the_rust_program
 
 本项目用于记录 rust 语言的学习过程
+
+## 使用 Cargo 命令
+
+本项目是一个 workspace 包含多个 package
+
+在 Workspace 根目录运行
+
+- cargo build: 构建 workspace 中的所有 packages。
+- cargo test: 运行 workspace 中所有 packages 的测试。
+- cargo run -p package_one: 运行名为 package_one 的二进制 package (如果它是一个可执行文件)。
+- cargo check: 检查 workspace 中所有 packages 的代码。
+- cargo clean: 清理所有 package 的构建产物。
+
+在特定 Package 目录运行
+
+- cd package_one
+- cargo build: 只构建 package_one。
+- cargo test: 只运行 package_one 的测试。
---
+++ closures_and_iterators/src/main.rs
@@ -0,0 +1,54 @@
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    // 在Option<T>上的unwrap_or_else方法，接受一个无参闭包作为参数，该闭包返回一个T类型的值
+    // 如果Option<T>的值是Some，则返回Some中的值，否则调用闭包并返回其结果
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+}
+
+fn main() {
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
+}
